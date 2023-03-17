160a3695b36e98b3c418b6c6238ddae135d55d9e
Quise <darkquise@gmail.com>
2023-03-16 20:34:55 -0400

    fetch stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 
 const urlRoot = 'http://ws.audioscrobbler.com/2.0/'
 const FMkey = '454e25c0ad504f5f95f870a78830824c';
-const genreSearch = `?method=tag.getinfo&tag=disco&api_key=${FMkey}&format=json`
+const genreSearch = `http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=disco&api_key=${FMkey}&format=json`
 
 // /2.0/?method=tag.gettoptracks&tag=disco&api_key=454e25c0ad504f5f95f870a78830824c&format=json
 
 
 const sharesSecret = 'c7b73866d4588addbb675a95ce264480'
-const url = urlRoot + genreSearch;
+
 const getAPI = (url) => {
 
     fetch(url)
@@ -19,4 +19,4 @@ const getAPI = (url) => {
 	.catch(err => console.error(err));
 }
 
-getAPI(url)
+getAPI(genreSearch)
