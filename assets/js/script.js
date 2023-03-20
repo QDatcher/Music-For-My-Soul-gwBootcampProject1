17fe7d06360fb55809ce7a15df477ce54917b9be
Quise <darkquise@gmail.com>
2023-03-20 01:20:48 -0400

    created 2 fmaAPI functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,8 @@
+// const FMkey = '454e25c0ad504f5f95f870a78830824c';
+// const sharesSecret = 'c7b73866d4588addbb675a95ce264480';
 
-const urlRoot = 'http://ws.audioscrobbler.com/2.0/'
-const FMkey = '454e25c0ad504f5f95f870a78830824c';
-const genreSearch = `http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=disco&api_key=${FMkey}&format=json`
+var genre = 'country'
 
-// /2.0/?method=tag.gettoptracks&tag=disco&api_key=454e25c0ad504f5f95f870a78830824c&format=json
-
-
-const sharesSecret = 'c7b73866d4588addbb675a95ce264480'
 
 const getAPI = (url) => {
 
@@ -19,4 +15,21 @@ const getAPI = (url) => {
 	.catch(err => console.error(err));
 }
 
-getAPI(genreSearch)
+
+
+const fmAPI = {
+	getTracksFromApi: (randomWord) =>{
+		var topTracksUrl = `http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=${randomWord}&api_key=454e25c0ad504f5f95f870a78830824c&format=json`;
+		getAPI(topTracksUrl)
+	},
+	getGenreTracksFromApi: (genre) =>{
+		var genreSearch = `http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=${genre}&api_key=454e25c0ad504f5f95f870a78830824c&format=json`;
+		getAPI(genreSearch)
+	}
+}
+
+
+
+fmAPI.getTracksFromApi('grey')
+
+// getAPI(fmAPI.topTracksUrl)
